7291966a94da39512103be131a3b47ec9922ed88
matthunterking <matthew85king@gmail.com>
2021-05-14 14:51:36 +0100

    Added contact links and icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,13 +19,50 @@ const IndexPage = () => (
     <h2>Developer</h2>
     <StaticImage
       src="../images/mattlogo.png"
-      quality={100}
       formats={["AUTO", "WEBP", "AVIF"]}
       alt="Matt's Logo"
-      style={{ marginBottom: `1.45rem` }}
+      style={{ marginBottom: `3rem` }}
       backgroundColor={'#4AE1BA'}
       placeholder={'none'}
     />
+    <div style={{
+      display: "flex",
+      justifyContent: 'space-between',
+      flexDirection: 'row',
+      alignItems: 'center',
+      width: '150px'
+    }}>
+      <a href='mailto:matthunterking@gmail.com' target="_blank">
+        <StaticImage
+          src="../images/email.png"
+          width={30}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="Email"
+          backgroundColor={'#4AE1BA'}
+          placeholder={'none'}
+        />
+      </a>
+      <a href='https://www.linkedin.com/in/matthunterking/' target="_blank">
+        <StaticImage
+          src="../images/linkedin-logo.png"
+          width={30}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="LinkedIn"
+          backgroundColor={'#4AE1BA'}
+          placeholder={'none'}
+        />
+      </a>
+      <a href='https://github.com/matthunterking' target="_blank">
+        <StaticImage
+          src="../images/github.png"
+          width={30}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="Github"
+          backgroundColor={'#4AE1BA'}
+          placeholder={'none'}
+        />
+      </a>
+    </div>
   </div >
 )
 
